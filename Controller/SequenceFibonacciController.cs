561c45497a79a1c31044aa1dc2c3e97b7fae9aad
joaovpinheiro <joaovpinheiros@gmail.com>
2026-07-13 20:30:45 -0300

    refactor: Remove Comentarios

--- Controller/SequenceFibonacciController.cs
+++ Controller/SequenceFibonacciController.cs
@@ -6,7 +6,6 @@ namespace Calculo_de_salario.Controller
         public int limitFibonaci { get; set; }
         public List<int> sequence = new();
 
-        // Verificar se o numero é ou não parte da sequencia Fibonaci
         public (bool status, string message) VerifyFibonacci(int number)
         {
             var eq1 = 5 * (Math.Pow(number, 2)) - 4;
@@ -19,7 +18,6 @@ namespace Calculo_de_salario.Controller
             return (false, $"O numero {number} não faz parte da sequencia Fibonaci");
         }
 
-        // Criar sequencia Fibonaci
         public string SequenceFibonacci()
         {
             int previus = 0;
@@ -37,12 +35,8 @@ namespace Calculo_de_salario.Controller
             return sequence;
         }
 
-        //Modificar valor do limite da lista
         public void SetLimiteFibonaci(int limit) => limitFibonaci = limit;
-        //Obter valor de limite da lista
         public int GetLimiteFibonaci() => limitFibonaci;
-
-        //Adicionar numero na sequencia
         public void AddNumberInSequence(int number) => sequence.Add(number);
     }
 }
